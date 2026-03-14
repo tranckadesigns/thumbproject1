47d6a6cebc96e98039f953c46694f2d32100b7cb
Rens Burgmans <rensburgmans01@gmail.com>
2026-03-14 14:40:19 +0800

    Fix Stripe API version to match installed SDK

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-02-25.clover",
   typescript: true,
 });
 
